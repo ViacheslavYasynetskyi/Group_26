0d620cca6ba12ceb2d7533039b53322e1864510c
ViacheslavYasynetskyi <slawa1594@gmail.com>
2022-02-20 21:08:25 +0000

    add

---
+++ home_works/CRUD_1/main_1.py
@@ -0,0 +1,46 @@
+from create_1 import create_user
+from read_1 import user_info, all_users_info
+user_emails = []
+users_storage = {}
+
+
+while True:
+    action = input("Please enter create or read, or update, or delete actions: ")
+
+    if action == "create":
+        print("action = ", action)
+
+        email = input("Email: ")
+        name = input("Name: ")
+        password = input("Password: ")
+        phone = input("Phone: ")
+
+        create_user(email,
+                    name,
+                    password,
+                    phone,
+                    user_emails,
+                    users_storage)
+        print("user_emails = ", user_emails)
+        print("users_storage = ", users_storage)
+
+    elif action == "read_all":
+        print("action = ", action)
+        all_users_info(users_storage)
+
+    elif action == "read_user":
+
+        user_e = input("Enter user email ")
+        message = user_info(user_e, user_emails, users_storage)
+
+        print("action = ", action)
+        print("User: ", message)
+
+    elif action == "update":
+        print("action = ", action)
+
+    elif action == "delete ":
+        print("action = ", action)
+
+    else:
+        print("Please enter create or read, or update, or delete")
---
+++ home_works/CRUD_1/read_1.py
@@ -0,0 +1,16 @@
+def user_info(email, user_emails, user_storage):
+
+    if email in user_emails:
+        message = ("User_email=", email, "\n", "User_info: ", user_storage[email])
+        return message
+    else:
+        message = "No user with email:", email
+        return message
+
+
+def all_users_info(users_storage):
+
+    for k, v in users_storage.items():
+        user_email = "User_email " + k
+        user_info_1 = "User_info", v
+        print(user_email, "\n", user_info_1)
